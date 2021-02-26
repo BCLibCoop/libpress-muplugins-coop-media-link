d7d02753beb9b9d67503ff6fa5bbee825ed672df
Sam Edwards <sam.edwards@bc.libraries.coop>
2021-02-26 10:14:03 -0800

    Code cleanup  - PSR12 standard  - Simplified multi-language handling  - Converted to shortcode from global function

--- coop-media-link.php
+++ coop-media-link.php
@@ -1,197 +1,196 @@
-<?php defined('ABSPATH') || die(-1);
+<?php
 
 /**
- * Plugin Name: Co-op Media Link
- * Description: Options to allow libraries to customize their digital media link in searchform. Installed as MUST USE.
- * Author: Jonathan Schatz, BC Libraries Cooperative
- * Author URI: https://bc.libraries.coop
- * Version: 0.1.0
- **/
+ * Coop Media Link
+ *
+ * A plugin to that adds a simple widget that takes text and a URL and displays
+ * it. Used to display a media link in the searchform. Supports multi-language.
+ *
+ * PHP Version 7
+ *
+ * @package           BCLibCoop\CoopMediaLink
+ * @author            Erik Stainsby <eric.stainsby@roaringsky.ca>
+ * @author            Ben Holt <ben.holt@bc.libraries.coop>
+ * @author            Jonathan Schatz <jonathan.schatz@bc.libraries.coop>
+ * @author            Sam Edwards <sam.edwards@bc.libraries.coop>
+ * @copyright         2013-2021 BC Libraries Cooperative
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Coop Media Link
+ * Description:       Options to allow libraries to customize their digital media link in searchform
+ * Version:           1.0.0
+ * Network:           true
+ * Requires at least: 5.2
+ * Requires PHP:      7.0
+ * Author:            BC Libraries Cooperative
+ * Author URI:        https://bc.libraries.coop
+ * Text Domain:       coop-media-link
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
 
-if ( ! class_exists( 'CoopMediaLink' )) :
+namespace BCLibCoop;
 
-	class CoopMediaLink {
+use function \pll_languages_list;
 
-		var $slug = 'coop-media-links';
+class CoopMediaLink
+{
+    private static $instance;
+    public $slug = 'coop-media-links';
+    public $languages;
 
-		public function __construct() {
-	
-			add_action( 'init', array( &$this, '_init' ));
-		}
+    public function __construct()
+    {
+        if (isset(self::$instance)) {
+            return;
+        }
 
-		public function _init() {
-				
-			if( is_admin()) {
-				add_action( 'admin_menu', array( &$this,'add_media_link_menu' ));
-				add_action( 'admin_post_coop_media_link_submit', array( $this, 'coop_media_link_save_change_callback' ));
-			}
+        self::$instance = $this;
 
-			//Set default options if not already set
-			if (function_exists('pll_languages_list')) {
+        // Dummy langauges option for when Polylang is not in use
+        $this->languages = [
+            (object) [
+                'locale' => '',
+                'name' => '',
+            ],
+        ];
 
-				// Polylang is enabled so loop through each configured language
-				$languages = pll_languages_list('fields');
-				foreach($languages as $curlang) {
-					if ($curlang->locale == 'en_CA') {
-						add_option ($this->slug . $curlang->locale . '-label-text', 'Download Digital Media', '', 'yes');
-						add_option ($this->slug . $curlang->locale . '-uri', '/research/download-digital-media', '', 'yes');
-					}
-				}
-			}
-			else {
-				add_option ($this->slug . '-label-text', 'Download Digital Media', '', 'yes');
-				add_option ($this->slug . '-uri', '/research/download-digital-media', '', 'yes');
-			}
-		
-		}
-	
-		public function add_media_link_menu() {
-			add_submenu_page( 'site-manager', 'Co-op Media Link', 'Media Link', 'manage_local_site', $this->slug, array(&$this,'admin_media_link_settings_page'));
-		}
+        add_action('init', array(&$this, 'init'));
+    }
 
-		public function coop_media_link_save_change_callback() {
-			// Check the nonce field, if it doesn't verify report error and stop
-			if (! isset( $_POST['coop_media_link_nonce']) || ! wp_verify_nonce( $_POST['coop_media_link_nonce'], 'coop_media_link_submit')) {
-				die('Sorry, there was an error handling your form submission.');
-			}
+    public function init()
+    {
+        // Check if polylang is available and if so use its list of languages
+        if (function_exists('pll_languages_list')) {
+            $this->languages = pll_languages_list('fields');
+        }
 
-			if (function_exists('pll_languages_list')) {
+        if (is_admin()) {
+            add_action('admin_menu', array(&$this, 'addMediaLinkMenu'));
+            add_action('admin_post_coop_media_link_submit', array($this, 'coopMediaLinkSaveChangeCallback'));
+        }
 
-				// Polylang is enabled so loop through each configured language
-				$languages = pll_languages_list('fields');
-				foreach($languages as $curlang) {
-					$link_text = sanitize_text_field($_POST[$this->slug . $curlang->locale . '-label-text']);
-					$link_uri = sanitize_text_field($_POST[$this->slug . $curlang->locale . '-uri']);
-					error_log($link_text . ',' . $link_uri);
-					update_option($this->slug . $curlang->locale . '-label-text', $link_text);
-					update_option($this->slug . $curlang->locale . '-uri', $link_uri);
-				}
-			}
-			else {
+        // Set default options if not already set
+        foreach ($this->languages as $curlang) {
+            add_option($this->slug . $curlang->locale . '-label-text', 'Download Digital Media', '', 'yes');
+            add_option($this->slug . $curlang->locale . '-uri', '/research/download-digital-media', '', 'yes');
+        }
 
-				$link_text = sanitize_text_field($_POST[$this->slug.'-label-text']);
-				$link_uri = sanitize_text_field($_POST[$this->slug.'-uri']);
+        add_shortcode('coop-media-link', [&$this, 'coopMediaLinkShortcode']);
+    }
 
-				error_log( $link_text .', '. $link_uri );
+    public function addMediaLinkMenu()
+    {
+        add_submenu_page(
+            'site-manager',
+            'Co-op Media Link',
+            'Media Link',
+            'manage_local_site',
+            $this->slug,
+            [&$this, 'adminMediaLinkSettingsPage']
+        );
+    }
 
-				update_option($this->slug.'-label-text',$link_text);
-				update_option($this->slug.'-uri',$link_uri);
-			}
+    public function coopMediaLinkSaveChangeCallback()
+    {
+        // Check the nonce field, if it doesn't verify report error and stop
+        if (!isset($_POST['coop_media_link_nonce'])
+            || !wp_verify_nonce($_POST['coop_media_link_nonce'], 'coop_media_link_submit')
+        ) {
+            wp_die('Sorry, there was an error handling your form submission.');
+        }
 
-			wp_redirect(admin_url('admin.php?page=coop-media-links'));
+        foreach ($this->languages as $curlang) {
+            $link_text = sanitize_text_field($_POST[$this->slug . $curlang->locale . '-label-text']);
+            $link_uri = sanitize_text_field($_POST[$this->slug . $curlang->locale . '-uri']);
 
+            update_option($this->slug . $curlang->locale . '-label-text', $link_text);
+            update_option($this->slug . $curlang->locale . '-uri', $link_uri);
+        }
 
-		}
+        wp_redirect(admin_url('admin.php?page=coop-media-links'));
+        exit;
+    }
 
-		/**
-		*	Store option to map URL to unqiue address 
-		*	per co-op client library
-		*	
-		**/
-		public function admin_media_link_settings_page() {
+    /**
+     * Store option to map URL to unqiue address
+     * per co-op client library
+     **/
+    public function adminMediaLinkSettingsPage()
+    {
+        if (!current_user_can('manage_local_site')) {
+            wp_die('You do not have required permissions to view this page');
+        }
 
-			if( ! current_user_can('manage_local_site') ) die('You do not have required permissions to view this page');
+        $out = [];
+        $out[] = '<div class="wrap">';
 
-			if (!function_exists('pll_languages_list')) {
-				$link_text = get_option($this->slug.'-label-text');
-				$link_uri = get_option($this->slug.'-uri');
-			}
+        $out[] = '<div id="icon-options-general" class="icon32">';
+        $out[] = '<br>';
+        $out[] = '</div>';
+        $out[] = '<h2>Media Link</h2>';
+        $out[] = '<form action="' . esc_url(admin_url('admin-post.php')) . '" method="post">';
+        $out[] = '<table class="form-table">';
 
-			$out = array();
-			$out[] = '<div class="wrap">';
+        foreach ($this->languages as $curlang) {
+            $link_text = stripslashes(get_option($this->slug . $curlang->locale . '-label-text'));
+            $link_uri = get_option($this->slug . $curlang->locale . '-uri');
+            $prefix = $this->slug . $curlang->locale;
 
-			$out[] = '<div id="icon-options-general" class="icon32">';
-			$out[] = '<br>';
-			$out[] = '</div>';
-			$out[] = '<h2>Media Link</h2>';
-			$out[] = '<form action="' . esc_url(admin_url('admin-post.php')) . '" method="post">';			
-			$out[] = '<table class="form-table">';
+            $out[] = '<tr valign="top">';
+            $out[] = '<th scope="row">';
+            $out[] = '<label for="' . $prefix . '-uri">' . $curlang->name . ' Media Link URI:</label>';
+            $out[] = '</th>';
+            $out[] = '<td>';
+            $out[] = '<input type="text" id="'.$prefix.'-uri" name="'.$prefix.'-uri"  value="'.$link_uri.'">';
+            $out[] = '</td>';
+            $out[] = '</tr>';
 
-			// Check if polylang is available and if so output a form field for each configured language
-			if (function_exists('pll_languages_list')) {
-				$languages = pll_languages_list('fields');
-				foreach($languages as $curlang) {
-					$link_text = stripslashes(get_option($this->slug . $curlang->locale . '-label-text'));
-					$link_uri = get_option($this->slug . $curlang->locale . '-uri');
+            $out[] = '<tr valign="top">';
+            $out[] = '<th scope="row">';
+            $out[] = '<label for="' . $prefix . '-label-text">' . $curlang->name . ' Media Link Label:</label>';
+            $out[] = '</th>';
+            $out[] = '<td>';
+            $out[] = '<input type="text" id="'.$prefix.'-label-text" name="'.$prefix.'-label-text"  value="'
+                     . $link_text.'">';
+            $out[] = '</td>';
+            $out[] = '</tr>';
+        }
 
+        $out[] = '</table>';
+        $out[] = '<p class="submit">';
+        $out[] = '<input type="hidden" name="action" value="coop_media_link_submit">';
+        $out[] = wp_nonce_field('coop_media_link_submit', 'coop_media_link_nonce');
+        $out[] = '<input type="submit" value="Save Changes" class="button button-primary" id="'
+                 . $this->slug . '-submit" name="submit">';
+        $out[] = '</p>';
+        $out[] = '</form>';
 
+        echo implode("\n", $out);
+    }
 
-					$out[] = '<tr valign="top">';
-					$out[] = '<th scope="row">';
-					$out[] = '<label for="' . $this->slug . $curlang->locale . '-uri">' . $curlang->name . ' Media Link URI:</label>';
-					$out[] = '</th>';
-					$out[] = '<td>';
-					$out[] = '<input type="text" id="' . $this->slug . $curlang->locale . '-uri" name="' . $this->slug . $curlang->locale . '-uri"  value="' . $link_uri . '">';
-					$out[] = '</td>';
-					$out[] = '</tr>';
+    /**
+     * Front-side shortcode callback
+     **/
+    public function coopMediaLinkShortcode()
+    {
+        // Check if polylang is available and if so get correct info for configured language
+        if (function_exists('pll_languages_list')) {
+            $link_text = stripslashes(get_option($this->slug . get_locale() . '-label-text'));
+            $link_uri = get_option($this->slug . get_locale() . '-uri');
+        } else {
+            $link_text = stripslashes(get_option($this->slug . '-label-text'));
+            $link_uri = get_option($this->slug . '-uri');
+        }
 
-					$out[] = '<tr valign="top">';
-					$out[] = '<th scope="row">';
-					$out[] = '<label for="' . $this->slug . $curlang->locale . '-label-text">' . $curlang->name . ' Media Link Label:</label>';
-					$out[] = '</th>';
-					$out[] = '<td>';
-					$out[] = '<input type="text" id="' . $this->slug . $curlang->locale . '-label-text" name="' . $this->slug . $curlang->locale . '-label-text"  value="' . $link_text . '">';
-					$out[] = '</td>';
-					$out[] = '</tr>';
-				}
-			}
-			else {
+        $link = '<a class="coop-media-link overdrive-link" href="' . $link_uri . '">' . $link_text . '</a>';
 
-				$out[] = '<tr valign="top">';
-				$out[] = '<th scope="row">';
-				$out[] = '<label for="'.$this->slug.'-uri">Media Link URI:</label>';
-				$out[] = '</th>';
-				$out[] = '<td>';
-				$out[] = '<input type="text" id="'.$this->slug.'-uri" name="'.$this->slug.'-uri"  value="'.$link_uri.'">';
-				$out[] = '</td>';
-				$out[] = '</tr>';
-
-
-				$out[] = '<tr valign="top">';
-				$out[] = '<th scope="row">';
-				$out[] = '<label for="'.$this->slug.'-label-text">Media Link Label:</label>';
-				$out[] = '</th>';
-				$out[] = '<td>';
-				$out[] = '<input type="text" id="'.$this->slug.'-label-text" name="'.$this->slug.'-label-text"  value="'.$link_text.'">';
-				$out[] = '</td>';	
-				$out[] = '</tr>';
-			}
-
-			$out[] = '</table>';
-			$out[] = '<p class="submit">';
-			$out[] = '<input type="hidden" name="action" value="coop_media_link_submit">';
-			$out[] = wp_nonce_field( 'coop_media_link_submit', 'coop_media_link_nonce' );
-			$out[] = '<input type="submit" value="Save Changes" class="button button-primary" id="'.$this->slug.'-submit" name="submit">';
-			$out[] = '</p>';
-			$out[] = '</form>';
-
-			echo implode("\n",$out);
-		}
-
-		/**
-		*Front-side widget callback
-		*
-		**/
-		public function coop_media_link_widget() {
-
-			// Check if polylang is available and if so get correct info for configured language
-			if (function_exists('pll_languages_list')) {
-				$link_text = stripslashes(get_option($this->slug . get_locale() . '-label-text'));
-				$link_uri = get_option($this->slug . get_locale() . '-uri');
-			}
-			else {
-				$link_text = stripslashes(get_option($this->slug.'-label-text'));
-				$link_uri = get_option($this->slug.'-uri');
-			}
-
-			$link = '<a class="coop-media-link overdrive-link" href="' . $link_uri . '">' .$link_text .'</a>';
-			return $link;
-		}
-
-	} //class
-
-if ( ! isset( $coopmedialink ) ) {
-	global $coopmedialink;
-	$coopmedialink = new CoopMediaLink();
+        return $link;
+    }
 }
-endif; /* ! class_exists */
+
+// No direct access
+defined('ABSPATH') || die(-1);
+
+$coopmedialink = new CoopMediaLink();
